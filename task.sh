3751be4cdb32d5d65995b447a190093fc58b49c7
Serhii Kermas <serhii.kermas@gmail.com>
2025-08-06 12:24:57 +0300

    Solution

--- task.sh
+++ task.sh
@@ -3,6 +3,11 @@
 DB_USER="${DB_USER}"
 DB_PASSWORD="${DB_PASSWORD}"
 
+if [[ -z "$DB_USER" || -z "$DB_PASSWORD" ]]; then
+  echo "Error: DB_USER and DB_PASSWORD environment variables must be set."
+  exit 1
+fi
+
 mysqldump -u "$DB_USER" -p"$DB_PASSWORD" ShopDB --no-create-db --result-file=ShopDBdb_nodb_backup.sql
 mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBReserve < ShopDBdb_nodb_backup.sql
 
